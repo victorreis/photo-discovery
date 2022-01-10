5db3d5b3efa0c5216772ff21a519b0d51430ffab
victorreis <v.crisostomo.reis@gmail.com>
2022-01-10 12:08:25 -0300

    chore: update the global test setup and update the App component tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,39 @@
-import { render, screen } from '@testing-library/react';
-
 import { App } from './App';
+import {
+  renderRTRCreator,
+  renderJestDomCreator,
+  screen,
+} from './Config/Tests/GlobalSetup.config';
+import { themes } from './Theme/CustomThemeProvider';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Learn React/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component tests', () => {
+  const setup = () => {
+    const renderRTR = () => renderRTRCreator(App, {});
+    const renderJestDom = () => renderJestDomCreator(App, {});
+
+    return { renderRTR, renderJestDom };
+  };
+
+  describe(`behavior tests`, () => {
+    it(`should render the Typography component`, () => {
+      setup().renderJestDom();
+      const testInstance = screen.getByTestId('container');
+
+      expect(testInstance).toBeTruthy();
+    });
+  });
+
+  describe(`style tests`, () => {
+    it(`should have style the Container component`, () => {
+      setup().renderJestDom();
+      const container = screen.getByTestId('container');
+
+      expect(container).toHaveStyle({
+        backgroundColor: themes.default.colors.background.default,
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden',
+      });
+    });
+  });
 });
