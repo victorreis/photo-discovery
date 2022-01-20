6ddb99aee2823f3dfbb6a565345ed55b1b52af96
victor.reis <victor.reis@iteris.com.br>
2022-01-19 23:06:32 -0300

    feat: style scrollbar to look like the website

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -6,6 +6,23 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${({ theme }) => theme.colors.background.default.normal};
+  }
+  &::-webkit-scrollbar-track {
+    background: ${({ theme }) => theme.colors.background.default.light};
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: ${({ theme }) => theme.colors.background.default.darker}cc;
+  }
+  &::-webkit-scrollbar-track:hover {
+    background: ${({ theme }) => theme.colors.background.default.light}cc;
+  }
 `;
 
 export const PageContainer = styled.div`
